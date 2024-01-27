fafbd4a5895a0126e351ecaea6c02de0bc5a3ecd
ar1622 <ar1622@live.mdx.ac.uk>
2024-01-28 03:26:06 +0600

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/",
+  base: "/vue.js-frontend",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
