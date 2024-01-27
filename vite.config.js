17beafbbab29c2e7fa3aed517429d3a77a50d63b
ar1622 <ar1622@live.mdx.ac.uk>
2024-01-27 18:11:58 +0600

    added base url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/vue.js-frontend",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
